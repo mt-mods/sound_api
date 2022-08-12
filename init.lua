497150953cd2d5a95d176d56bf00f6eec3dfd94c
Olivier Dragon <odragon@protonmail.com>
2022-08-12 00:02:07 -0400

    Refactor function to override sounds using sound_api

--- init.lua
+++ init.lua
@@ -2,33 +2,26 @@ local modpath = minetest.get_modpath("sound_api")
 
 sound_api = dofile(modpath .. "/sound_api_core/init.lua")
 
+local function override_sounds(name, def)
+    if name and def and def._sound_def and def._sound_def.key then
+        if sound_api[def._sound_def.key] then
+            minetest.override_item(name, {
+                sounds = sound_api[def._sound_def.key](def._sound_def.input)
+            })
+        else
+            minetest.log("warning", "missing sound_api function for node _sound_def key: "..def._sound_def.key)
+        end
+    end
+end
+
 minetest.register_on_mods_loaded(function()
     for name, def in pairs(minetest.registered_nodes) do
-        if def._sound_def and def._sound_def.key then
-            if sound_api[def._sound_def.key] then
-                minetest.override_item(name, {
-                    sounds = sound_api[def._sound_def.key](def._sound_def.input)
-                })
-            else
-                print("missing sound_api function for sound def key: "..def._sound_def.key)
-                minetest.log("warning", "missing sound_api function for sound def key: "..def._sound_def.key)
-            end
-        end
+        override_sounds(name, def)
     end
 
     local old_reg_node = minetest.register_node
     function minetest.register_node(name, def)
-        if def._sound_def and def._sound_def.key then
-            if sound_api[def._sound_def.key] then
-                minetest.override_item(name, {
-                    sounds = sound_api[def._sound_def.key](def._sound_def.input)
-                })
-            else
-                print("missing sound_api function for sound def key: "..def._sound_def.key)
-                minetest.log("warning", "missing sound_api function for sound def key: "..def._sound_def.key)
-            end
-        end
-
+        override_sounds(name, def)
         old_reg_node(name, def)
     end
 end)
